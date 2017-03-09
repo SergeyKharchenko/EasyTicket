8e11f2278e86ee36182005b4fa9d5dc6cb3f4717
Serhii <hsvlis4@gmail.com>
2017-03-09 22:21:54 +0200

    Add Core project

---
+++ EasyTicket.WebApp/Controllers/RequestController.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace EasyTicket.WebApp.Controllers {
+    public class RequestController : Controller {
+        public IActionResult Index() {
+            return View();
+        }
+    }
+}
