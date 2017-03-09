75ace1f08d98469d8f0729e79667183dfb4ed7c8
Serhii <hsvlis4@gmail.com>
2017-03-09 22:21:54 +0200

    Add Core project

---
+++ EasyTicket.WebApp/webpack.config.js
@@ -0,0 +1,58 @@
+﻿var path = require('path');
+var webpack = require("webpack");
+
+var rootPath = "wwwroot";
+var buildFolder = "build";
+
+module.exports = {
+    context: path.resolve(rootPath),
+    entry: ["./entryPoint"],
+    output: {
+        path: path.resolve(path.join(rootPath, buildFolder)),
+        publicPath: buildFolder + '/',
+        filename: "bundle.js"
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.jsx?$/,
+                exclude: /node_modules/,
+                loader: "babel-loader",
+                query: {
+                    presets: ['es2015', 'react']
+                }
+            },
+            {
+                test: /\.css?$/,
+                exclude: /node_modules/,
+                loader: "style-loader!css-loader"
+            },
+            {
+                test: /\.scss?$/,
+                exclude: /node_modules/,
+                loader: "style-loader!css-loader!sass-loader"
+            },
+            {
+                test: /\.(png|jpg|jpeg|gif|svg|woff|woff2|eot|ttf)?$/,
+                exclude: /node_modules/,
+                loader: "url-loader?limit=10000"
+            }
+        ]
+    },
+    resolve: {
+        extensions: ['.js', '.jsx', '.css', '.scss'],
+        alias: {
+            'jquery': path.resolve('node_modules/jquery/dist/jquery'),
+            'materialize-scss': path.resolve('node_modules/materialize-css/sass/materialize.scss')
+        }
+    },
+    plugins: [
+        new webpack.ProvidePlugin({
+            $: 'jquery',
+            jQuery: "jquery",
+            'window.$': 'jquery',
+            'window.jQuery': 'jquery'
+        })
+    ],
+    devtool: 'source-map'
+};
---
+++ EasyTicket.WebApp/wwwroot/entryPoint.jsx
@@ -0,0 +1,14 @@
+﻿// styles
+import './styles/site'
+//import './css/1'
+
+// scripts
+// ReSharper disable once UnusedLocals: Needed internaly
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+// start
+$(function() {
+    console.log('loaded');
+    //ReactDOM.render(<ApprovalApp errorCode={approvalData.errorCode} token={approvalData.token} />, document.getElementById('content'));
+});
