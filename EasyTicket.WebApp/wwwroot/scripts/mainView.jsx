2c70a244cf22d1029805d72ed767cc9bcca35433
Serhii <hsvlis4@gmail.com>
2017-03-10 23:47:13 +0200

    select station component init

--- EasyTicket.WebApp/wwwroot/scripts/mainView.jsx
+++ EasyTicket.WebApp/wwwroot/scripts/mainView.jsx
@@ -1,4 +1,5 @@
 ﻿import React from 'react';
+import StationSelect from './stationSelect'
 
 export default class MainView extends React.Component {
     constructor(props) {
@@ -9,6 +10,14 @@ export default class MainView extends React.Component {
         return (
             <div>
                 <p>This is Main</p>
+                <div className='row'>
+                    <div className='col s6'>
+                        <StationSelect label='Станция отправления'/>
+                    </div>
+                    <div className='col s6'>
+                        <StationSelect label='Станция прибытия'/>
+                    </div>
+                </div>
                 <p><a href='/#/request'>Go to Request page</a></p>
             </div>
         )
