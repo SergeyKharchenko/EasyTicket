09b511eeac8813abae35f1061c18416790713cec
Serhii <hsvlis4@gmail.com>
2017-03-10 22:32:14 +0200

    routing

--- EasyTicket.WebApp/wwwroot/entryPoint.jsx
+++ EasyTicket.WebApp/wwwroot/entryPoint.jsx
@@ -3,12 +3,11 @@ import './styles/site'
 //import './css/1'
 
 // scripts
-// ReSharper disable once UnusedLocals: Needed internaly
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Application from './scripts/application';
 
 // start
 $(function() {
-    console.log('loaded');
-    //ReactDOM.render(<ApprovalApp errorCode={approvalData.errorCode} token={approvalData.token} />, document.getElementById('content'));
+    ReactDOM.render(<Application />, document.getElementById('container'));
 });
---
+++ EasyTicket.WebApp/wwwroot/scripts/requestView.jsx
@@ -0,0 +1,16 @@
+﻿import React from 'react';
+
+export default class RequestView extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <div>
+                <p>This is Request</p>
+                <p><a href='/#/main'>Go to Main page</a></p>
+            </div>
+        )
+    }
+}
