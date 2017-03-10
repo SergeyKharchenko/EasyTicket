103206e03f39051f023c1856a4394d99390de680
Serhii <hsvlis4@gmail.com>
2017-03-10 23:47:13 +0200

    select station component init

---
+++ EasyTicket.WebApp/wwwroot/scripts/stationSelect.jsx
@@ -0,0 +1,35 @@
+﻿import React from 'react';
+import 'materialize-css';
+
+export default class StationSelect extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            loading: false
+        }
+
+        this.minLength = 3;
+    }
+
+    componentDidMount() {
+       
+    }
+
+    render() {
+        return (
+            <div className={"input-field" + (this.state.loading ? ' loading' : '')}>
+                <input id="stationFrom" type="text" className='validate' onChange={this.onChange.bind(this)}/>            
+                <label htmlFor="stationFrom">{this.props.label}</label>
+            </div>
+        )
+    }
+
+    onChange(e) {
+        var value = e.target.value;
+        if (value.length < this.minLength) {
+            this.setState({loading: false});
+            return;
+        }
+        this.setState({loading: true});
+    }
+}
