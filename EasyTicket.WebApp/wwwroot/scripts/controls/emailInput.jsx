dccef6f8e0fd7513c70ca79149a2714dd751fa3d
Serhii <hsvlis4@gmail.com>
2017-03-12 22:02:41 +0200

    email

--- EasyTicket.WebApp/wwwroot/scripts/controls/emailInput.jsx
+++ EasyTicket.WebApp/wwwroot/scripts/controls/emailInput.jsx
@@ -10,8 +10,8 @@ export default class EmailInput extends React.Component {
         return (
             <div>
                 <i className="material-icons prefix">email</i>
-                <input id={this.id} type="text" className="validate" />
-                <label htmlFor={this.id}>{this.props.label}</label>
+                <input id={this.id} type="email" className="validate" />
+                <label htmlFor={this.id} data-error="Неверный формат">{this.props.label}</label>
             </div>
         );
     }
